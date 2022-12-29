2e7ee4a17f75981319a83f5221b3c76544d51d88
JoeyThe <56653916+JoeyThe@users.noreply.github.com>
2022-12-28 19:17:12 -0800

    Just a commit for some new files ^.^

--- MainWindow.py
+++ MainWindow.py
@@ -15,33 +15,73 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QGridLayout, QMainWindow, QMenuBar,
-    QPushButton, QSizePolicy, QStatusBar, QTextEdit,
+from PySide6.QtWidgets import (QApplication, QFrame, QGridLayout, QMainWindow,
+    QMenuBar, QPushButton, QRadioButton, QSizePolicy,
+    QSpacerItem, QStatusBar, QTextEdit, QVBoxLayout,
     QWidget)
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(800, 600)
+        MainWindow.resize(708, 605)
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
-        self.gridLayout = QGridLayout(self.centralwidget)
+        self.gridLayout_2 = QGridLayout(self.centralwidget)
+        self.gridLayout_2.setObjectName(u"gridLayout_2")
+        self.frame = QFrame(self.centralwidget)
+        self.frame.setObjectName(u"frame")
+        self.frame.setMinimumSize(QSize(500, 500))
+        self.frame.setMaximumSize(QSize(500, 500))
+        self.frame.setFrameShape(QFrame.StyledPanel)
+        self.frame.setFrameShadow(QFrame.Raised)
+        self.gridLayout = QGridLayout(self.frame)
         self.gridLayout.setObjectName(u"gridLayout")
-        self.pushButton = QPushButton(self.centralwidget)
-        self.pushButton.setObjectName(u"pushButton")
+        self.verticalLayout = QVBoxLayout()
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.verticalSpacer = QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding)
 
-        self.gridLayout.addWidget(self.pushButton, 0, 0, 1, 1)
+        self.verticalLayout.addItem(self.verticalSpacer)
 
-        self.textEdit = QTextEdit(self.centralwidget)
+        self.radioButton = QRadioButton(self.frame)
+        self.radioButton.setObjectName(u"radioButton")
+
+        self.verticalLayout.addWidget(self.radioButton)
+
+        self.radioButton_2 = QRadioButton(self.frame)
+        self.radioButton_2.setObjectName(u"radioButton_2")
+
+        self.verticalLayout.addWidget(self.radioButton_2)
+
+        self.radioButton_3 = QRadioButton(self.frame)
+        self.radioButton_3.setObjectName(u"radioButton_3")
+
+        self.verticalLayout.addWidget(self.radioButton_3)
+
+        self.verticalSpacer_2 = QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding)
+
+        self.verticalLayout.addItem(self.verticalSpacer_2)
+
+
+        self.gridLayout.addLayout(self.verticalLayout, 0, 0, 1, 1)
+
+        self.textEdit = QTextEdit(self.frame)
         self.textEdit.setObjectName(u"textEdit")
 
         self.gridLayout.addWidget(self.textEdit, 0, 1, 1, 1)
 
+        self.pushButton = QPushButton(self.frame)
+        self.pushButton.setObjectName(u"pushButton")
+
+        self.gridLayout.addWidget(self.pushButton, 1, 1, 1, 1)
+
+
+        self.gridLayout_2.addWidget(self.frame, 0, 0, 1, 1)
+
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QMenuBar(MainWindow)
         self.menubar.setObjectName(u"menubar")
-        self.menubar.setGeometry(QRect(0, 0, 800, 22))
+        self.menubar.setGeometry(QRect(0, 0, 708, 22))
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QStatusBar(MainWindow)
         self.statusbar.setObjectName(u"statusbar")
@@ -54,6 +94,9 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.radioButton.setText(QCoreApplication.translate("MainWindow", u"Red", None))
+        self.radioButton_2.setText(QCoreApplication.translate("MainWindow", u"Green", None))
+        self.radioButton_3.setText(QCoreApplication.translate("MainWindow", u"Blue", None))
         self.pushButton.setText(QCoreApplication.translate("MainWindow", u"PushButton", None))
     # retranslateUi
 
--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from PySide6 import QtWidgets
 
 from MainWindow import Ui_MainWindow
 
-
 class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
@@ -12,7 +11,21 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.pushButton.clicked.connect(self.magic)
 
     def magic(self):
-        self.textEdit.append("Michael Gay ")
+        if self.radioButton.isChecked():
+            self.changeTextEditTextColor("red")
+            self.textEdit.append("Michael Gay in red")
+        elif self.radioButton_2.isChecked():
+            self.changeTextEditTextColor("green")
+            self.textEdit.append("Michael Gay in green")
+        elif self.radioButton_3.isChecked():
+            self.changeTextEditTextColor("blue")
+            self.textEdit.append("Michael Gay in blue")
+        else:
+            self.changeTextEditTextColor("black")
+            self.textEdit.append("Michael Gay")
+
+    def changeTextEditTextColor(self, color):
+        self.textEdit.setTextColor(f"{color}")
 
 app = QtWidgets.QApplication(sys.argv)
 
